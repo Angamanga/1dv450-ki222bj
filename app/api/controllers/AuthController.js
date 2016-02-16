802444a1d5d301a68c22ea9c922d0d6562670830
Angamanga <ki222bj@student.lnu.se>
2016-02-16 16:28:06 +0100

    redirecting user if wrong user credentials

--- app/api/controllers/AuthController.js
+++ app/api/controllers/AuthController.js
@@ -2,21 +2,16 @@
 let passport = require('passport');
 
 module.exports = {
-
     _config: {
         actions: false,
         shortcuts: false,
         rest: false
     },
-
     login: function(req, res) {
-
         passport.authenticate('local', function(err, user, info) {
             if ((err) || (!user)) {
-                return res.send({
-                    message: info.message,
-                    user: user
-                });
+                return res.redirect('/login');
+
             }
             req.logIn(user, function(err) {
                 if (err) res.send(err);
@@ -25,10 +20,8 @@ module.exports = {
                     user: user
                 });
             });
-
         })(req, res);
     },
-
     logout: function(req, res) {
         req.logout();
         res.redirect('/');
