d0661eb4377785d36e7a92676e08c5d392a0869b
Angamanga <ki222bj@student.lnu.se>
2016-02-17 23:20:11 +0100

    created signup form

--- app/api/controllers/UserController.js
+++ app/api/controllers/UserController.js
@@ -1,17 +1,18 @@
-/**
- * UserController.js 
- * 
- * @module      :: Controller
- * @description :: Provides the base user
- *                 actions used to make waterlock work.
- *                 
- * @docs        :: http://waterlock.ninja/documentation
- */
+module.exports = {
+'new'(req,res){
+  res.view();
+},
+  create: function(req,res,next){
+    User.create(req.params.all(), function userCreated(err,user){
+      if (req.param("password") != req.param("confirmation")){
+        return next("Passwords are not matching");
+      }
+      if(err){
+        req.flash('err',err.ValidationError);
+        return res.redirect('/user/new');
+      }
+      return res.json(user);
+    })
+  }
+};
 
-module.exports = require('waterlock').actions.user({
-  /* e.g.
-    action: function(req, res){
-  
-    }
-  */
-});
--- app/api/models/User.js
+++ app/api/models/User.js
@@ -1,21 +1,16 @@
-/**
- * User
- *
- * @module      :: Model
- * @description :: This is the base user model
- * @docs        :: http://waterlock.ninja/documentation
- */
-
 module.exports = {
-
-  attributes: require('waterlock').models.user.attributes({
-    
-    /* e.g.
-    nickname: 'string'
-    */
-    
-  }),
-  
-  beforeCreate: require('waterlock').models.user.beforeCreate,
-  beforeUpdate: require('waterlock').models.user.beforeUpdate
+  schema: true,
+  attributes: {
+    email: {
+      type: 'string',
+      email: true,
+      required: true,
+      unique: true
+    },
+    encryptedPassword: {
+      type: 'string'
+    }
+  }
 };
+
+
