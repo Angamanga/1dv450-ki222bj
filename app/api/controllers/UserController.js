abf24abe00a4456b6ee6447a803f2fc6086c513e
Angamanga <ki222bj@student.lnu.se>
2016-02-18 09:38:26 +0100

    server validation registration form

--- app/api/controllers/UserController.js
+++ app/api/controllers/UserController.js
@@ -2,13 +2,13 @@ module.exports = {
 'new'(req,res){
   res.view();
 },
-  create: function(req,res,next){
+  create(req,res,next){
     User.create(req.params.all(), function userCreated(err,user){
       if (req.param("password") != req.param("confirmation")){
         return next("Passwords are not matching");
       }
       if(err){
-        req.flash('err',err.ValidationError);
+        req.session.flash = {'err':err};
         return res.redirect('/user/new');
       }
       return res.json(user);
