34b1895e1984fb59e40ea61908b1c19081235c57
Angamanga <ki222bj@student.lnu.se>
2016-02-22 21:58:00 +0100

    adding mongodb

--- app/api/models/User.js
+++ app/api/models/User.js
@@ -1,3 +1,5 @@
+"use strict";
+
 module.exports = {
   schema: true,
   attributes: {
@@ -12,8 +14,26 @@ module.exports = {
     },
     encryptedPassword: {
       type: 'string'
+    },
+    toJSON(){
+      let obj = this.toObject();
+      delete obj.password;
+      delete obj.confirmation;
+      delete obj.encryptedPassword;
+      delete obj._csrf;
+      return obj;
     }
-  }
+  },
+  beforeCreate(values,next){
+    if(!values.password || values.password !== values.confirmation){
+      return(next({err:['Passwords does not match password confirmation']}));
+    }
+    require('bcrypt').hash(values.password, 10, (err, encryptedPassword)=>{
+      if(err) return next(err);
+      values.encryptedPassword = encryptedPassword;
+      next();
+    });
+    }
 };
 
 
