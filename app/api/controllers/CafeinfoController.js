3b68ee52aa56dbc05f6c85b53cb9c14a3faf0b5f
Angamanga <ki222bj@student.lnu.se>
2016-04-01 10:54:15 +0200

    continuing with geosearch, need regex

--- app/api/controllers/CafeinfoController.js
+++ app/api/controllers/CafeinfoController.js
@@ -26,7 +26,8 @@ module.exports = {
     let query = {},
       distance;
 
-    //todo: refactor 27-32 in own function
+    //todo: refactor 29-4 in own function
+    req.param('id') ? query.id = req.param('id') : null;
     req.param('name') ? query.name = (req.param('name')) : null;
     req.param('streetAddress') ? query.streetAddress = req.param('streetAddress') : null;
     req.param('postalCode') ? query.postalCode = req.param('postalCode') : null;
@@ -35,18 +36,39 @@ module.exports = {
     req.param('wifi') ? query.wifi = req.param('wifi') : null;
     req.param('latitude') ? query.latitude = req.param('latitude') : null;
     req.param('longitude') ? query.longitude = req.param('longitude') : null;
-    req.param('distance') ? distance = req.param('distance') : 500;
   //https://github.com/balderdashy/sails-mongo/issues/46
+    if(req.param('latitude')&&req.param('longitude')) {
+      //regex for lat/long!
+      let maxDistance = req.param('distance') ? req.param('distance') : 500;
+      query.coordinates = {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [
+              parseInt(req.param('longitude')),
+              parseInt(req.param('latitude'))
+            ]
+          },
+          $maxDistance: maxDistance
+        }
+      }
+    }
+    else if(req.param('latitude') || req.param('longitude')) {
+      return res.badRequest('you must enter both latitude and longitude');
+    }
 
-    Cafeinfo.find(query).exec((err,cafe)=>{
-      if(err){
+    Cafeinfo.native((err,collection)=>{
+      collection.find(query).toArray((err,cafe)=>{
+        console.log(err);
+        if(err){
+
         return res.badRequest('no cafe with that id was found');
       }
       else{
         return res.send(['200'], _.orderBy(cafe, ['createdAt'], ['desc']));
       }
     });
-  },
+  });},
   'badRequest'(req,res,next){
     return res.badRequest('Invalid request');
   }
